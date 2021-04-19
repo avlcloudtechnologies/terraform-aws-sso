c6ccb6299576fc23c4e55234beeef0eb639452a0
aurimasmick <aurimasmic@gmail.com>
2021-04-19 17:05:59 +0100

    Remove comments

--- variables.tf
+++ variables.tf
@@ -1,23 +1,3 @@
-# variable "permission_sets" {
-#   type = list(object({
-#     name             = string
-#     description      = string
-#     relay_state      = string
-#     session_duration = string
-#     tags             = map(string)
-#     inline_policy    = string
-#     managed_policies = list(string)
-#   }))
-
-#   default = []
-# }
-# variable "permission_sets" {
-#   default = {}
-# }
-# variable "account_assignments" {
-#   default = []
-# }
-
 variable "permission_sets" {
   description = "Map of maps containing Permission Set names as keys. See permission_sets description in README for information about map values."
   type        = any
