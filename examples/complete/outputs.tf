555a0a0941faebcd33d322ab956aeeafdcb2b86f
haarchri <chhaar30@googlemail.com>
2021-08-10 10:24:40 +0200

    fix(example): fix output & generated Readme (#5)
    Signed-off-by: Christopher Haar <chhaar30@googlemail.com>

--- examples/complete/outputs.tf
+++ examples/complete/outputs.tf
@@ -3,7 +3,7 @@ output "aws_ssoadmin_permission_sets" {
   value       = module.sso.aws_ssoadmin_permission_sets
 }
 
-output "aws_ssoadmin_permission_sets" {
+output "aws_ssoadmin_account_assignments" {
   description = "Maps of account assignments to permission sets with keys user/group_name.permission_set_name.account_id and attributes listed in Terraform resource aws_ssoadmin_account_assignment documentation."
   value       = module.sso.aws_ssoadmin_account_assignments
 }
