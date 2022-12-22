e8af265b6c3ec32b324099c78f73a311550a32db
Aurimas Mickevicius <aurimasmick@users.noreply.github.com>
2022-12-22 07:05:13 +0000

    Update deprecated identitystore filter (#11)

--- main.tf
+++ main.tf
@@ -39,18 +39,22 @@ data "aws_ssoadmin_instances" "this" {}
 data "aws_identitystore_group" "this" {
   for_each          = toset(local.groups)
   identity_store_id = tolist(data.aws_ssoadmin_instances.this.identity_store_ids)[0]
-  filter {
-    attribute_path  = "DisplayName"
-    attribute_value = each.value
+  alternate_identifier {
+    unique_attribute {
+      attribute_path  = "DisplayName"
+      attribute_value = each.value
+    }
   }
 }
 
 data "aws_identitystore_user" "this" {
   for_each          = toset(local.users)
   identity_store_id = tolist(data.aws_ssoadmin_instances.this.identity_store_ids)[0]
-  filter {
-    attribute_path  = "UserName"
-    attribute_value = each.value
+  alternate_identifier {
+    unique_attribute {
+      attribute_path  = "UserName"
+      attribute_value = each.value
+    }
   }
 }
 
--- versions.tf
+++ versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.30"
+      version = ">= 4.34"
     }
   }
 }
